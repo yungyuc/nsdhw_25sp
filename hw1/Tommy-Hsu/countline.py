277b230ab3429e8d235d5decd1143f7ce3dfd11d
Tommy-Hsu <hpc880214@gmail.com>
2025-03-03 02:12:52 +0000

    hear from class

--- hw1/Tommy-Hsu/countline.py
+++ hw1/Tommy-Hsu/countline.py
@@ -1,20 +1,2 @@
-#!/usr/bin/env bash
-
-exec ${PYTHON_BIN} -c "
-import sys
-import os.path
-
-
-if len(sys.argv) < 2:
-    sys.stdout.write('missing file name\n')
-elif len(sys.argv) > 2:
-    sys.stdout.write('only one argument is allowed\n')
-else:
-    fname = sys.argv[1]
-    if os.path.exists(fname):
-        with open(fname) as fobj:
-            lines = fobj.readlines()
-        sys.stdout.write('{} lines in {}\n'.format(len(lines), fname))
-    else:
-        sys.stdout.write('{} not found\n'.format(fname))
-" "$@"
+#!/bin/bash
+exec ${PYTHON_BIN} -c "import sys; print(len(open(sys.argv[1]).readlines()))" "$@"
--- hw1/Tommy-Hsu/countline.sh
+++ hw1/Tommy-Hsu/countline.sh
@@ -3,7 +3,6 @@
 # This script counts the number of lines in a file.
 #
 # Setup
-export PYTHON_BIN
 
 # Actions
 wc -l $1 | tr -dc '0-9'
