c7b33ed934890fd1c029b059058a970e2b3191fd
mingchunhsu046 <fodsic24.cs13@nycu.edu.tw>
2025-03-04 22:11:54 +0800

    first try

---
+++ hw1/mingchunhsu046/countline.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import subprocess
+
+python_bin = os.getenv("PYTHON_BIN","python3")
+
+try:
+    subprocess.run([python_bin,"--version"], check=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+except FileNotFoundError:
+    print(f"exec: {python_bin}: not found", file=sys.stderr)
+    sys.exit(1)
+if len(sys.argv) < 2:
+    sys.stdout.write('missing file name\n')
+elif len(sys.argv) > 2:
+    sys.stdout.write('only one argument is allowed\n')
+else:
+    fname = sys.argv[1]
+    if os.path.exists(fname):
+        with open(fname) as fobj:
+            lines = fobj.readlines()
+        sys.stdout.write('{} lines in {}\n'.format(len(lines), fname))
+    else:
+        sys.stdout.write('{} not found\n'.format(fname))
